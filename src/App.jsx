3077fd42bff2acb6f87fd25e9af6daf27b9752e1
minhhoang2904 <buileminhhoang2005@gmail.com>
2025-09-07 10:53:53 -0400

    Upload entire project

---
+++ src/App.jsx
@@ -0,0 +1,21 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import { Home } from './pages/Home'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <BrowserRouter>
+        <Routes>
+          <Route index element={<Home />}/>
+          <Route path="*" element={<NotFound />}/>
+        </Routes>
+      </BrowserRouter>
+    </>
+  )
+}
+
+export default App
